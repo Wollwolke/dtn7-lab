5eaf993da16c2b56296ac22ac1611a4adfcbf385
Wollwolke <17803954+Wollwolke@users.noreply.github.com>
2023-04-13 11:21:02 +0200

    fixed ros build script

--- shared/build_ros.sh
+++ shared/build_ros.sh
@@ -2,6 +2,8 @@
 
 rm -rf /shared/ros_ws/
 cp -a /tmp/shared/ros_ws/ /shared/
+rm -rf /shared/ros_ws/build /shared/ros_ws/install /shared/ros_ws/log
 
 cd /shared/ros_ws
-colcon build --cmake-clean-cache --packages-select dtn_proxy --cmake-args -DCMAKE_BUILD_TYPE=Release
+
+colcon build --cmake-clean-cache --packages-up-to dtn_proxy --cmake-args -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF
