a4a42810ae1e410c3368f6dec1443db8106d55b2
Wollwolke <17803954+Wollwolke@users.noreply.github.com>
2023-04-14 00:55:36 +0200

    clab: workaround for broken core-automator

--- clab.sh
+++ clab.sh
@@ -39,6 +39,7 @@ podman run -it --rm \
     -e DISPLAY \
     -v $SHARED:/shared \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    -v /home/lars/Documents/mt/core-automator/coreposlib.py:/usr/local/bin/coreposlib.py \
     -v ~/Documents/mt/ros_ws/:/tmp/shared/ros_ws/ \
     -v ~/Documents/mt/dtn7-lab/custom_services/:/tmp/shared/custom_services \
     -v ~/Documents/mt/bags:/shared/bags \
