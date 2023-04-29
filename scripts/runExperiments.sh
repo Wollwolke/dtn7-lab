4bb5fe219c64e8f9070eabbb49cf5c9be8273bb6
Wollwolke <17803954+Wollwolke@users.noreply.github.com>
2023-04-29 02:18:59 +0200

    added experiments launch script

---
+++ scripts/runExperiments.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+# robocup
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup/circle/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup/circle_double/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup/circle_small/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup/cross/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup/crosshatch/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup/crosshatch_large/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup/follow/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup/parallel/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup/parallel_large/experiment.conf
+
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_baseline/circle/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_baseline/circle_double/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_baseline/circle_small/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_baseline/cross/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_baseline/crosshatch/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_baseline/crosshatch_large/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_baseline/follow/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_baseline/parallel/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_baseline/parallel_large/experiment.conf
+
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noCombining/circle/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noCombining/circle_double/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noCombining/circle_small/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noCombining/cross/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noCombining/crosshatch/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noCombining/crosshatch_large/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noCombining/follow/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noCombining/parallel/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noCombining/parallel_large/experiment.conf
+
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noCompression/circle/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noCompression/circle_double/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noCompression/circle_small/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noCompression/cross/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noCompression/crosshatch/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noCompression/crosshatch_large/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noCompression/follow/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noCompression/parallel/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noCompression/parallel_large/experiment.conf
+
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noImageCompression/circle/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noImageCompression/circle_double/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noImageCompression/circle_small/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noImageCompression/cross/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noImageCompression/crosshatch/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noImageCompression/crosshatch_large/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noImageCompression/follow/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noImageCompression/parallel/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noImageCompression/parallel_large/experiment.conf
+
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noDeletion/circle/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noDeletion/circle_double/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noDeletion/circle_small/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noDeletion/cross/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noDeletion/crosshatch/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noDeletion/crosshatch_large/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noDeletion/follow/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noDeletion/parallel/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/robocup_noDeletion/parallel_large/experiment.conf
+
+# delay
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/one_hop_delay/mtcp/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/one_hop_delay/tcp/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/ros_delay/experiment.conf
+
+# industrial
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/industrial/industrial0/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/industrial/industrial1/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/industrial/industrial2/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/industrial/industrial3/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/industrial/industrial4/experiment.conf
+
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/industrial_baseline/industrial0/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/industrial_baseline/industrial1/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/industrial_baseline/industrial2/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/industrial_baseline/industrial3/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/industrial_baseline/industrial4/experiment.conf
+
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/industrial_noCombining/industrial0/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/industrial_noCombining/industrial1/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/industrial_noCombining/industrial2/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/industrial_noCombining/industrial3/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/industrial_noCombining/industrial4/experiment.conf
+
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/industrial_noCompression/industrial0/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/industrial_noCompression/industrial1/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/industrial_noCompression/industrial2/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/industrial_noCompression/industrial3/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/industrial_noCompression/industrial4/experiment.conf
+
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/industrial_noDeletion/industrial0/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/industrial_noDeletion/industrial1/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/industrial_noDeletion/industrial2/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/industrial_noDeletion/industrial3/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/industrial_noDeletion/industrial4/experiment.conf
+
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/industrial_noImageCompression/industrial0/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/industrial_noImageCompression/industrial1/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/industrial_noImageCompression/industrial2/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/industrial_noImageCompression/industrial3/experiment.conf
+~/Documents/mt/dtn7-lab/clab.sh ~/Documents/mt/dtn7-lab/shared/ /shared/scenarios/industrial_noImageCompression/industrial4/experiment.conf
